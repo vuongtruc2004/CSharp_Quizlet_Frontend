0230fd8b635f382ee5d423290489275b6d5dab90
NhanPhan <ptn987654321@gmail.com>
2025-08-24 09:14:34 +0700

    Clean folder

--- app/(student)/book/page.tsx
+++ app/(student)/book/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
-import BookSearch from "@/features/book/book.search";
-import BookList from "@/features/book/book.list";
+import BookSearch from "@/features/book/ui/book.search";
+import BookList from "@/features/book/ui/book.list";
 
 export const metadata: Metadata = {
     title: "Sách",
